2baa7bebad44521998d8eda905c0cd82382b4006
Zack YL Shih <zackyl.shih@moxa.com>
2015-01-21 10:37:40 +0800

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,13 @@ set -e
 ARCH=${ARCH:-armhf}
 COMMIT_BUILD=${COMMIT_BUILD:-1}
 BUILD_DIR=${BUILD_DIR:-`pwd`}
+IMAGE=${IMAGE:-sanji/python-dev:$ARCH}
 
 echo "* Get latest docker image"
-docker pull sanji/python-dev:$ARCH
+docker pull $IMAGE
 
 echo "* Build bundles dependencies into $BUILD_DIR/*.whl package"
-docker run --rm -v $BUILD_DIR:/data sanji/python-dev:$ARCH
+docker run --rm -v -u $BUILD_DIR:/data $IMAGE
 
 if [ $COMMIT_BUILD == "1" ]; then
 	git add build/* && git commit build/* -m "Build @ `date +'%Y.%m.%d %H:%M:%S'`"
